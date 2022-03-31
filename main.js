0bea56686546d48dfab7221d45ebf0280626a9cc
luyi <luyi@luyinoMacBook-Air.local>
2022-04-01 01:16:07 +0900

    L3-end

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            descriptions: 'light and comfortable. \n Made in japan',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://google.co.jp'
         }
     }
 })
